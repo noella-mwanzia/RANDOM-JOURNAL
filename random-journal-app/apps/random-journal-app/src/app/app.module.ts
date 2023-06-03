c46ad62b5ff949212b93920e0314e984ad169d9e
Noella <noellamwanzia@gmail.com>
2023-06-03 07:49:13 +0300

    chore: initialise firebase project on the client

--- random-journal-app/apps/random-journal-app/src/app/app.module.ts
+++ random-journal-app/apps/random-journal-app/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { AngularFireModule } from '@angular/fire/compat';
+
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
