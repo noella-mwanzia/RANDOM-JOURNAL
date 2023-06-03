82893f213b15619382eef9741477bdf01ae410d2
Noella <noellamwanzia@gmail.com>
2023-06-03 16:08:24 +0300

    chore: import and declare needed modules.

--- random-journal-app/apps/random-journal-app/src/app/app.module.ts
+++ random-journal-app/apps/random-journal-app/src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+
 import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
@@ -10,9 +17,15 @@ import { environment } from '../environments/environment';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
 
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+
   ],
   providers: [],
   bootstrap: [AppComponent],
--- random-journal-app/libs/features/src/lib/features.module.ts
+++ random-journal-app/libs/features/src/lib/features.module.ts
@@ -3,7 +3,27 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { featuresRoutes } from './lib.routes';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { RegistrationComponent } from './auth/components/registration/registration.component';
+
+
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(featuresRoutes), RouterModule],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    RouterModule.forChild(featuresRoutes), RouterModule],
+
+    declarations: [RegistrationComponent]
 })
 export class FeaturesModule {}
