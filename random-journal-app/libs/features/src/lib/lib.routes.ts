3a5ff042c53cc81d1397dfc4ffc0a681e12d041a
Noella <noellamwanzia@gmail.com>
2023-06-04 09:52:20 +0300

    chore: set up routes

--- random-journal-app/libs/features/src/lib/lib.routes.ts
+++ random-journal-app/libs/features/src/lib/lib.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
+import { RegistrationComponent } from './auth/components/registration/registration.component';
+import { LogInComponent } from './auth/components/login/login.component';
+import { JournalEditorComponent } from './journals/components/editor/editor.component';
+import { TimelineComponent } from './journals/components/timeline/timeline.component';
+import { JournalPreviewComponent } from './journals/components/preview/preview.component';
 
-export const featuresRoutes: Route[] = [];
+export const featuresRoutes: Route[] = [
+    {path: '', component: LogInComponent},
+    {path:'register', component: RegistrationComponent},
+    {path:'timeline', component: TimelineComponent},
+    // {path:'', component: JournalEditorComponent}
+    // {path:'', component:JournalPreviewComponent}
+
+
+];
