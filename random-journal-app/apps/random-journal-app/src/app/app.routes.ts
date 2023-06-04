e210c6ecf9c51057a1be17581412c84db3706c9c
Noella <noellamwanzia@gmail.com>
2023-06-04 09:41:29 +0300

    chore: implement lazy loading from root module

--- random-journal-app/apps/random-journal-app/src/app/app.routes.ts
+++ random-journal-app/apps/random-journal-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+{
+    path: '',
+    loadChildren: () => import('libs/features/src/lib/features.module').then(m => m.FeaturesModule),
+
+}
+
+];
